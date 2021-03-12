0a64126c2defa36370519cce2391b1bb34c56f3b
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:39:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-    updateDOM ()
+    changeIDText ()
   });
 
-  function updateDOM() {
+  function changeIDText() {
     document.getElementById( "text" )
       .textContent = "This is really cool!";
   }
