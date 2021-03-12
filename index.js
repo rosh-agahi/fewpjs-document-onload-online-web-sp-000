0e5aa2d484a040490d17686b3f636c2ae13e26cd
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:40:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeIDText ()
+  });
+
+  function changeIDText() {
+    document.getElementById( "text" )
+      .textContent = "This is really cool!";
+  }
